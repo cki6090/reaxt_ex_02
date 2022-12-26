c20d4458eb10b1c4db2bb203c3d64ecad5fd725a
cki6090 <52525423+cki6090@users.noreply.github.com>
2022-12-26 15:33:33 +0900

    test2

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-6 bg-yellow-500 text-blue-500">hello world 테일 붙이기 완료~!</h1>
+    </div>
+  );
+}
+
+export default App;
