a3c564fad213fd2e58fbcabd0e46b24ec37ea9dd
cki6090 <52525423+cki6090@users.noreply.github.com>
2022-12-26 15:37:06 +0900

    221226

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="m-6 bg-yellow-500 text-blue-500">hello world 테일 붙이기 완료~!</h1>
+      <h1 className="m-6 bg-yellow-500 text-blue-500">hello world 테일 붙이기 완료~!~!</h1>
     </div>
   );
 }
